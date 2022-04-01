320a7c96051620a890c21c06d1406c0e564a39f8
Artem Danilaints <artem@daniliants.com>
2022-04-01 14:04:20 +0300

    Added tests

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import requests
 from flask import Flask, render_template
 from db.data import get_taxes
 from api import generate_json
-import json
 
 app = Flask(__name__)
 
@@ -14,21 +13,9 @@ def api_app():
 def index():
     return render_template('index.html')
 
-@app.route('/recipes')
-def recipes():
-
-    reqUrl = "https://api.spoonacular.com/recipes/complexSearch?apiKey=de9eefef5b62421fbc904960928291be&equipment=pan"
-
-    headersList = {
-    "Accept": "*/*",
-    "User-Agent": "Thunder Client (https://www.thunderclient.com)" 
-    }
-
-    payload = ""
-
-    response = requests.request("GET", reqUrl, data=payload,  headers=headersList)
-
-    return render_template('recipes.html', recipes=json.loads(response.text))
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
